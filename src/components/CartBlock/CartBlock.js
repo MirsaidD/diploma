59f3a1a359f8dd6b1bd4133bf63250111c78f5b7
MirsaidD <dzumaevmirsaid25@gmail.com>
2022-06-20 14:24:44 +0600

    add basket

--- src/components/CartBlock/CartBlock.js
+++ src/components/CartBlock/CartBlock.js
@@ -1,12 +1,15 @@
 import { BiCartAlt } from 'react-icons/bi'
-
+import { useSelector } from 'react-redux/';
 import './CartBlock.css'
 
 function CartBlock() {
+    const items = useSelector(state => state.cart.itemsInCart);
+    const totalPrice = items.reduce((acc, game) => acc += game.price, 0)
+
     return (
         <div className="Cart-block">
             <BiCartAlt size={40} className="cart-block__icon" />
-            <span className='cart-block__total-price'>2232руб</span>
+            <span className='cart-block__total-price'>{totalPrice}$.</span>
         </div>
     );
 }
--- src/components/GameBuy/GameBuy.js
+++ src/components/GameBuy/GameBuy.js
@@ -1,9 +1,18 @@
 import { Button } from "../button/button"
+import { useDispatch } from "react-redux/";
+import { setItemInCart } from "../../redux/cart/reducer";
+
 export const GameBuy = ({ game }) => {
+    const dispatch = useDispatch();
+
+    const handleClick = (e) => {
+        e.stopPropagation();
+        dispatch(setItemInCart(game));
+    }
     return (
-        <div className="Game-Buy">
+        <div className="Game-Buy" >
             <span className="game-buy__price">{game.price}$.</span>
-            <Button type="primery" onClick={() => null} >В Корзину</Button>
-        </div>
+            <Button type="primery" onClick={(handleClick)} >В Корзину</Button>
+        </div >
     )
 }
